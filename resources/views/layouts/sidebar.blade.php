99a3ca65948e932c3b0ccf133a5bd0bebc197e38
lee-pc <leecherome.revil@robinsonsland.com>
2023-11-09 14:08:49 +0800

    fix(sidebar) Remove unecessary sidebar item

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -53,22 +53,6 @@
                                     <i class="dripicons-warning"></i><span> Late Time </span>
                                 </a>
                             </li>
-                            <li class="">
-                                <a href="/leave" class="waves-effect {{ request()->is("leave") || request()->is("leave/*") ? "mm active" : "" }}">
-                                    <i class="dripicons-backspace"></i> <span> Leave </span>
-                                </a>
-                            </li>
-                            <li class="">
-                                <a href="/overtime" class="waves-effect {{ request()->is("overtime") || request()->is("overtime/*") ? "mm active" : "" }}">
-                                    <i class="dripicons-alarm"></i> <span> Over Time </span>
-                                </a>
-                            </li>
-                            <li class="menu-title">Tools</li>
-                            <li class="">
-                                <a href="{{ route("finger_device.index") }}" class="waves-effect {{ request()->is("finger_device") || request()->is("finger_device/*") ? "mm active" : "" }}">
-                                    <i class="fas fa-fingerprint"></i> <span> Biometric Device </span>
-                                </a>
-                            </li>
 
                         </ul>
 
